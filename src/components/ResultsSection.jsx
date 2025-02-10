b9af8f9df4662ede2d52e5d7e1402e24a6abac7b
Kamran Kiani <kiani.km@gmail.com>
2025-02-10 16:32:45 +0330

    img path bug fixes

--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -3,7 +3,7 @@ function ResultsSection() {
     <div className="flex flex-col gap-200 bg-slate-900 px-300 py-400 text-center md:p-500 lg:justify-center lg:rounded-es-[80px]">
       <img
         className="mx-auto"
-        src="/public/assets/images/illustration-empty.svg"
+        src="/assets/images/illustration-empty.svg"
         alt=""
       />
       <h2 className="text-600 text-white">Results shown here</h2>
