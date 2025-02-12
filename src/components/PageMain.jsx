f1630118288b4e90edd9f6e6c9436fdc81fb5dc4
Kamran Kiani <kiani.km@gmail.com>
2025-02-12 16:50:29 +0330

    Bug fixes

--- src/components/PageMain.jsx
+++ src/components/PageMain.jsx
@@ -26,7 +26,7 @@ function PageMain() {
   }
   return (
     <main className="flex-1 md:flex md:items-center">
-      <div className="mx-auto grid max-w-[43rem] bg-white md:my-500 md:overflow-hidden md:rounded-3xl lg:max-w-[63em] lg:grid-cols-2">
+      <div className="mx-auto grid max-w-[43rem] bg-white drop-shadow-[0_32px_64px_hsla(202,55,16,10)] md:my-500 md:overflow-hidden md:rounded-3xl lg:max-w-[63em] lg:grid-cols-2">
         <CalculatorSection
           mortgageData={mortgageData}
           onCalculate={handleCalculation}
